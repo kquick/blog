48b960c35cf1b2e4ce0758ddb4fdf95318414880
Kevin Quick <kquick@galois.com>
2020-12-19 22:43:07 -0800

    Generator and style updates.

--- do/blog.el
+++ do/blog.el
@@ -2,3 +2,4 @@
 (setq org-html-htmlize-output-type 'css)
 (setq org-html-htmlize-convert-nonascii-to-entities nil)
 (setq org-html-htmlize-html-charset "utf-8")
+(setq org-html-head-include-default-style nil)
--- do/gen_html.sh
+++ do/gen_html.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env nix-shell
 # -*- mode: shell; -*-
 #! nix-shell -i bash -p "pkgs.emacs27WithPackages (p: [p.htmlize p.writegood-mode])"
-echo hi
+set -x
 emacs -batch --load do/blog.el --visit=$1 --funcall org-html-export-to-html
-echo done
